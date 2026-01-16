5cb7799e4fc845629838081ae3b93756424a3426
Rafael LLamas <rafael.llamas@etu-u.bordeaux.fr>
2026-01-16 11:18:54 +0100

    j'ai joué

--- tests/Test_Bordel_Flint.c
+++ tests/Test_Bordel_Flint.c
@@ -1,9 +1,12 @@
 // pour les arbres
 #include "flint/arb.h"
-// pour les polys
+// pour les polys classique
 #include "flint/fmpz_poly.h"
 // pour les commandes flint
 #include "flint/flint.h"
+// ATTENTION la on rentre dans le modulo n
+#include "flint/fmpz_mod.h"
+#include "flint/fmpz_mod_poly.h"
 
 /*
 OK donc ici c'est mon terrain de jeu ou je vais ecrire des chose qui peuvent ne
@@ -35,27 +38,82 @@ void naive_Poly_test() {
   // ici un int non signé au coeff 0
   fmpz_poly_set_coeff_ui(poly2, 3, 12);
   fmpz_poly_set_coeff_ui(poly1, 5, 2);
+  flint_printf("poly1 :\n");
   fmpz_poly_print(poly1);
   flint_printf("\n");
   fmpz_poly_init(poly3);
   // permet de faire une multiplication naive
   fmpz_poly_mul_classical(poly3, poly1, poly2);
   fmpz_poly_mul_classical(poly1, poly1, poly2);
-
+  flint_printf("poly1 fois poly2:\n");
   fmpz_poly_print(poly1);
   flint_printf("\n");
+  flint_printf("poly2 :\n");
   fmpz_poly_print(poly2);
   flint_printf("\n");
 
-  // mets le poly a 0
-
+  flint_printf("poly3 :\n");
   fmpz_poly_print(poly3);
   flint_printf("\n");
-  flint_printf("%d\n", fmpz_poly_equal(poly1, poly3));
+  flint_printf("equal = 1, 0 sinon :%d\n", fmpz_poly_equal(poly1, poly3));
+  // mets le poly a 0
   fmpz_poly_zero(poly1);
+  flint_printf("1 si vraie, 0 sinon :%d\n", fmpz_poly_is_zero(poly1));
+  flint_printf("poly2 fois 2^15 :\n");
+  // il existe des variants pour tout type de multiplication ou addition avec
+  // une constante
+  fmpz_poly_scalar_mul_2exp(poly1, poly2, 15);
+  fmpz_poly_print(poly1);
+  flint_printf("\n");
   fmpz_poly_clear(poly1);
   fmpz_poly_clear(poly2);
   fmpz_poly_clear(poly3);
+}
+
+void naive_mod_poly_test() {
+  /* C'est concretement les memes fonctions que la version générique de poly
+   * sauf que la il faut donc le modulo n et que chaque nom de fonction a mod
+   * qui apparait en plus !!! ATTENTION !!! il y a vachement moins de fonctions
+   * que pour le poly général donc moins de chose faisable*/
+  // tes poly particuliers
+  fmpz_mod_poly_t poly1, poly2, poly3;
+  // ton n pour le modulo
+  fmpz_mod_ctx_t n;
+  // toujours init
+  fmpz_mod_ctx_init_ui(n, 12);
+  fmpz_mod_poly_init(poly1, n);
+  fmpz_mod_poly_init(poly2, n);
+  fmpz_mod_poly_init(poly3, n);
+  // Pas de si donc faut ui
+  fmpz_mod_poly_set_coeff_ui(poly1, 2, 8, n);
+  fmpz_mod_poly_set_coeff_ui(poly2, 0, 2, n);
+
+  fmpz_mod_poly_set_coeff_ui(poly2, 3, 12, n);
+  fmpz_mod_poly_set_coeff_ui(poly1, 5, 2, n);
+  flint_printf("poly1 :\n");
+  flint_printf("x = %{fmpz_mod_poly} (%{fmpz_mod_ctx})\n", poly1, n);
+  fmpz_mod_poly_init(poly3, n);
+  // ne s'appelle plus classical. Pourquoi ??? JE NE SAIS PAS MOI il faut
+  // demander au prof
+  fmpz_mod_poly_mul(poly3, poly1, poly2, n);
+  fmpz_mod_poly_mul(poly1, poly1, poly2, n);
+  flint_printf("poly1 fois poly2:\n");
+  flint_printf("x = %{fmpz_mod_poly} (%{fmpz_mod_ctx})\n", poly1, n);
+  flint_printf("poly2 :\n");
+  flint_printf("x = %{fmpz_mod_poly} (%{fmpz_mod_ctx})\n", poly2, n);
+
+  flint_printf("poly3 :\n");
+  flint_printf("x = %{fmpz_mod_poly} (%{fmpz_mod_ctx})\n", poly3, n);
+  flint_printf("equal = 1, 0 sinon :%d\n",
+               fmpz_mod_poly_equal(poly1, poly3, n));
+
+  fmpz_mod_poly_zero(poly1, n);
+  flint_printf("1 si vraie, 0 sinon :%d\n", fmpz_mod_poly_is_zero(poly1, n));
+
+  fmpz_mod_poly_clear(poly1, n);
+  fmpz_mod_poly_clear(poly2, n);
+  fmpz_mod_poly_clear(poly3, n);
+  fmpz_mod_ctx_clear(n);
 }
 
 int main() {
@@ -71,4 +129,5 @@ int main() {
   // comme mpfr dans la logique
   arb_clear(x);
   naive_Poly_test();
+  naive_mod_poly_test();
 }
