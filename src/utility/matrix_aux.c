802f3069876fdc2c339b2ec60860f2142fe59eb3
Rafael LLamas <rafael.llamas@etu-u.bordeaux.fr>
2026-02-05 20:30:57 +0100

    matrices générer de facon random check

--- src/utility/matrix_aux.c
+++ src/utility/matrix_aux.c
@@ -8,62 +8,166 @@
 #include <stdlib.h>
 #include <time.h>
 
-int gr_mat_random_manual(gr_mat_t D, gr_ctx_t ctx) { return gr_not_implemented(); }
+int gr_mat_random_manual(gr_mat_t D, flint_rand_t state, ulong limit, gr_ctx_t ctx) {
+  /*
+  mets dans une matrices déja initialiser (voir pour init la matrice direct dans la fonction) des éléments random (<
+  limit) au position (i,j)
+  */
+  int error;
+  for (int i = 0; i < gr_mat_nrows(D, ctx); i++) {
+    for (int j = 0; j < gr_mat_ncols(D, ctx); j++) {
+      error = gr_set_ui(gr_mat_entry_ptr(D, i, j, ctx), n_randint(state, limit), ctx);
+      if (error != 0) return error;
+    }
+  }
+
+  return 0;
+}
 
 /*
 La matrice LU doit avoir U et L avec la partie sup qui doit etre U sur M ligne et des 0 apres, L est toute la partie
 triangulaire basse
 */
 int gr_mat_lu_detach(gr_mat_t L, gr_mat_t U, gr_mat_t LU, gr_ctx_t ctx) {
+  int error;
   for (int i = 0; i < gr_mat_ncols(LU, ctx); i++) {
     for (int j = i; j < gr_mat_ncols(LU, ctx); j++) {
-      gr_set(gr_mat_entry_ptr(U, i, j, ctx), gr_mat_entry_srcptr(LU, i, j, ctx), ctx);
+      error = gr_set(gr_mat_entry_ptr(U, i, j, ctx), gr_mat_entry_srcptr(LU, i, j, ctx), ctx);
+      if (error != 0) return error;
     }
   }
   for (int i = 0; i < gr_mat_nrows(LU, ctx); i++) {
     for (int j = 0; j < gr_mat_ncols(LU, ctx); j++) {
-      if (i == j) { gr_mat_set_ui(gr_mat_entry_ptr(L, i, j, ctx), 1, ctx); }
+      if (i == j) {
+        error = gr_mat_set_ui(gr_mat_entry_ptr(L, i, j, ctx), 1, ctx);
+        if (error != 0) return error;
+      }
     }
   }
   for (int i = 1; i < gr_mat_nrows(LU, ctx); i++) {
-    for (int j = 0; j < i; j++) { gr_set(gr_mat_entry_ptr(L, i, j, ctx), gr_mat_entry_srcptr(LU, i, j, ctx), ctx); }
+    for (int j = 0; j < i; j++) {
+      error = gr_set(gr_mat_entry_ptr(L, i, j, ctx), gr_mat_entry_srcptr(LU, i, j, ctx), ctx);
+      if (error != 0) return error;
+    }
   }
-  return gr_not_implemented();
+  return 0;
 }
 
 // TODO - might integrate with cmake tests later
 int test_matrix_aux() {
   flint_printf("*----------* Matrix Auxilary Test *----------*\n");
   gr_ctx_t ctx;
-  gr_mat_t A, LU, L, U, B;
+  gr_mat_t A, LU, L, U, B, C;
   slong rank, *P;
+  flint_rand_t state;
+  flint_rand_init(state);
   gr_ctx_init_nmod(ctx, 1009);
   P = flint_malloc(sizeof(slong) * 10);
   gr_mat_init(A, 5, 10, ctx);
-  gr_mat_init_set(B, A, ctx);
+  gr_mat_init(C, 10, 10, ctx);
+  int error = gr_mat_init_set(B, A, ctx);
+  if (error != 0) {
+    gr_mat_clear(A, ctx);
+    gr_mat_clear(B, ctx);
+    gr_mat_clear(C, ctx);
+    flint_rand_clear(state);
+    flint_free(P);
+    gr_ctx_clear(ctx);
+    return error;
+  }
   gr_mat_init(LU, 5, 10, ctx);
   gr_mat_init(L, 5, 10, ctx);
   gr_mat_init(U, 10, 10, ctx);
-  int error = rand_quasi_toeplitz(A, 5, 10, 0, ctx);
+  error = rand_quasi_toeplitz(A, 5, 10, 0, ctx);
+  if (error != 0) {
+    gr_mat_clear(A, ctx);
+    gr_mat_clear(B, ctx);
+    gr_mat_clear(C, ctx);
+    gr_mat_clear(LU, ctx);
+    gr_mat_clear(L, ctx);
+    gr_mat_clear(U, ctx);
+    flint_rand_clear(state);
+    flint_free(P);
+    gr_ctx_clear(ctx);
+    return error;
+  }
   flint_printf("A = \n");
   gr_mat_print(A, ctx);
   flint_printf("\n**************************************************\n");
   flint_printf("LU = \n");
   error = gr_mat_lu(&rank, P, LU, A, 0, ctx);
+  if (error != 0) {
+    gr_mat_clear(A, ctx);
+    gr_mat_clear(B, ctx);
+    gr_mat_clear(C, ctx);
+    gr_mat_clear(LU, ctx);
+    gr_mat_clear(L, ctx);
+    gr_mat_clear(U, ctx);
+    flint_rand_clear(state);
+    flint_free(P);
+    gr_ctx_clear(ctx);
+    return error;
+  }
   gr_mat_print(LU, ctx);
   flint_printf("\n**************************************************\n");
-  int res = gr_mat_lu_detach(L, U, LU, ctx);
+  error = gr_mat_lu_detach(L, U, LU, ctx);
+  if (error != 0) {
+    gr_mat_clear(A, ctx);
+    gr_mat_clear(B, ctx);
+    gr_mat_clear(C, ctx);
+    gr_mat_clear(LU, ctx);
+    gr_mat_clear(L, ctx);
+    gr_mat_clear(U, ctx);
+    flint_rand_clear(state);
+    flint_free(P);
+    gr_ctx_clear(ctx);
+    return error;
+  }
   flint_printf("U = \n");
   gr_mat_print(U, ctx);
   flint_printf("\n\n");
   flint_printf("L = \n");
   gr_mat_print(L, ctx);
   flint_printf("\n**************************************************\n");
-  gr_mat_mul(B, L, U, ctx);
+  error = gr_mat_mul(B, L, U, ctx);
+  if (error != 0) {
+    gr_mat_clear(A, ctx);
+    gr_mat_clear(B, ctx);
+    gr_mat_clear(C, ctx);
+    gr_mat_clear(LU, ctx);
+    gr_mat_clear(L, ctx);
+    gr_mat_clear(U, ctx);
+    flint_rand_clear(state);
+    flint_free(P);
+    gr_ctx_clear(ctx);
+    return error;
+  }
   flint_printf("B = \n");
   gr_mat_print(B, ctx);
-  flint_printf("\n");
+  flint_printf("\n**************************************************\n");
+  error = gr_mat_random_manual(C, state, 1009, ctx);
+  if (error != 0) {
+    gr_mat_clear(A, ctx);
+    gr_mat_clear(B, ctx);
+    gr_mat_clear(C, ctx);
+    gr_mat_clear(LU, ctx);
+    gr_mat_clear(L, ctx);
+    gr_mat_clear(U, ctx);
+    flint_rand_clear(state);
+    flint_free(P);
+    gr_ctx_clear(ctx);
+    return error;
+  }
+  flint_printf("C = \n");
+  gr_mat_print(C, ctx);
   gr_mat_clear(A, ctx);
   gr_mat_clear(LU, ctx);
-  return res;
+  gr_mat_clear(L, ctx);
+  gr_mat_clear(U, ctx);
+  gr_mat_clear(B, ctx);
+  gr_mat_clear(C, ctx);
+  flint_rand_clear(state);
+  flint_free(P);
+  gr_ctx_clear(ctx);
+  return 0;
 }
--- src/utility/matrix_aux.h
+++ src/utility/matrix_aux.h
@@ -6,7 +6,7 @@
 #include "flint/gr_mat.h"
 #include "flint/gr_poly.h"
 
-int gr_mat_random_manual(gr_mat_t D, gr_ctx_t ctx);
+int gr_mat_random_manual(gr_mat_t D, flint_rand_t state, ulong limit, gr_ctx_t ctx);
 
 int gr_mat_lu_detach(gr_mat_t L, gr_mat_t U, gr_mat_t LU, gr_ctx_t ctx);
 
