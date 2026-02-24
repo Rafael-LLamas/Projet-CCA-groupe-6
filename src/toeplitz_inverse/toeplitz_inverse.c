cb0d470b47af4d2c7fdf04ce662dc17866779228
Arda Barut <aliardabarut@gmail.com>
2026-02-24 12:07:31 +0100

    inverse base case & parameter fix

--- src/toeplitz_inverse/toeplitz_inverse.c
+++ src/toeplitz_inverse/toeplitz_inverse.c
@@ -1,18 +1,39 @@
+#include "addition.h"
 #include "displacement_matrices.h"
 #include "flint/flint.h"
 #include "flint/gr.h"
 #include "flint/gr_mat.h"
 #include "flint/gr_poly.h"
-#include "matrix_aux.h"
-#include "random_toeplitz.h"
+#include "multiplication.h"
 
 /*
-Arda's Notes:
-It is mentioned in the book that we can use Strassen's Algorithm
-to inverse a matrix.
-
 Algorithm 10.1 Algorithme de type Strassen pour inverser une matrice
 quasi-Toeplitz from [1].
+
+Only square and non singular matrices are invertible. We will reject
+any input of a non square matrix.
 */
 
-int gr_toeplitz_inverse(gr_mat_t G_D, gr_mat_t H_D, gr_mat_t G_A, gr_mat_t H_D, gr_ctx_t ctx) { return GR_SUCCESS; }
+int gr_toeplitz_inverse(gr_mat_t G_D, gr_mat_t H_D, gr_mat_t G_A, gr_mat_t H_A, gr_ctx_t ctx) {
+  int res = GR_SUCCESS;
+  // TODO: add a check of its determinant once implemented the toeplitz version (det must not be zero)
+
+  // base case
+  if (gr_mat_nrows(G_A, ctx) == 1) {
+    // Gd[0, 0] = 1, Gd[0, 1] = Ga[0, 0] * Ha[0, 0]
+    // Hd[0, 0] = 1, Hd[0, 1] = 0
+    gr_one(gr_mat_entry(G_D, 0, 0, ctx), ctx);
+    gr_mul(gr_mat_entry(G_D, 0, 1, ctx), gr_mat_entry(G_A, 0, 0, ctx), gr_mat_entry(H_A, 0, 0, ctx), ctx);
+    gr_one(gr_mat_entry(H_D, 0, 0, ctx), ctx);
+    gr_zero(gr_mat_entry(H_D, 0, 1, ctx), ctx);
+
+    // if rank > 2 better to put everything else to 0
+    for (slong i = 2; i < gr_mat_ncols(G_A, ctx); i++) {
+      gr_zero(gr_mat_entry(G_D, 0, i, ctx), ctx);
+      gr_zero(gr_mat_entry(H_D, 0, i, ctx), ctx);
+    }
+    return res;
+  }
+
+  return res;
+}
