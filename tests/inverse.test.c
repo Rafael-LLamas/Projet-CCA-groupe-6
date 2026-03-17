800550fd9570064861fd4cf1caf87154c69b0723
Arda <arda@Ardas-MacBook-Air.local>
2026-03-17 22:58:32 +0100

    Better test inverse_base_case

--- tests/inverse.test.c
+++ tests/inverse.test.c
@@ -6,51 +6,53 @@
 #include "flint/flint.h"
 #include "flint/gr.h"
 #include "flint/gr_mat.h"
+#include "flint/gr_types.h"
+#include "flint/ulong_extras.h"
 #include "toeplitz_inverse.h"
 
 int test_inverse_base_case() {
-  int res = GR_SUCCESS;
-  gr_ctx_t ctx;
-  gr_ctx_init_nmod(ctx, 17);
-  slong n = 1;
-  slong rank = 2;
-  gr_mat_t G_A, H_A, G_D, H_D;
-  gr_mat_init(G_A, n, rank, ctx);
-  gr_mat_init(H_A, n, rank, ctx);
-  gr_mat_init(G_D, n, rank, ctx);
-  gr_mat_init(H_D, n, rank, ctx);
-  res = gr_set_si(gr_mat_entry_ptr(G_A, 0, 0, ctx), 3, ctx);
-  res = gr_set_si(gr_mat_entry_ptr(G_A, 0, 1, ctx), -1, ctx);
-  res = gr_set_si(gr_mat_entry_ptr(H_A, 0, 0, ctx), 2, ctx);
-  res = gr_set_si(gr_mat_entry_ptr(H_A, 0, 1, ctx), 1, ctx);
-  flint_printf("Testing Strassen Inverse Base Case (n=1) over nmod 17\n");
-  flint_printf("---------------------------------------------------\n");
-  flint_printf("Input G_A: ");
-  gr_mat_print(G_A, ctx);
-  flint_printf("\nInput H_A: ");
-  gr_mat_print(H_A, ctx);
-  flint_printf("\n\n");
-  int out = gr_toeplitz_inverse(G_D, H_D, G_A, H_A, ctx);
-  if (out == GR_SUCCESS) {
-    flint_printf("[SUCCESS] Matrix inverted.\n");
-    flint_printf("Expected scalar: 3*2 + (-1)*1 = 5\n");
-    flint_printf("Expected inverse of 5 mod 17: 7\n\n");
+  int i = 0;
+  int status = GR_SUCCESS;
+  flint_rand_t state;
+  flint_rand_init(state);
 
-    flint_printf("Output G_D: ");
-    gr_mat_print(G_D, ctx); // Should be [7, 0]
-    flint_printf("\nOutput H_D: ");
-    gr_mat_print(H_D, ctx); // Should be [1, 0]
-    flint_printf("\n");
-  } else {
-    flint_printf("[ERROR] Failed to invert matrix.\n");
-    res = GR_TEST_FAIL;
+  gr_ctx_t ctx;
+  gr_ctx_init_nmod(ctx, n_randprime(state, 64, 1));
+
+  gr_mat_t A, A_inv, G_A, H_A, G_D, H_D, Check;
+  gr_mat_init(A, 1, 1, ctx);
+  gr_mat_init(A_inv, 1, 1, ctx);
+  gr_mat_init(G_A, 1, 1, ctx);
+  gr_mat_init(H_A, 1, 1, ctx);
+  gr_mat_init(G_D, 1, 1, ctx);
+  gr_mat_init(H_D, 1, 1, ctx);
+  gr_mat_init(Check, 1, 1, ctx);
+
+  while (i < 200) {
+    status |= gr_mat_zero(A, ctx);
+    do { status |= gr_mat_randtest(A, state, ctx); } while (gr_mat_is_zero(A, ctx) == T_TRUE);
+    status |= gr_mat_G_H(G_A, H_A, A, DISP_PLUS, ctx);
+    status |= gr_toeplitz_inverse(G_D, H_D, G_A, H_A, ctx);
+    status |= gr_mat_reconstruct_A(A_inv, G_D, H_D, DISP_PLUS, ctx);
+    status |= gr_mat_mul(Check, A, A_inv, ctx);
+
+    if (gr_mat_is_one(Check, ctx) != T_TRUE) {
+      status = GR_TEST_FAIL;
+      break;
+    }
+    i++;
   }
+
+  gr_mat_clear(A, ctx);
+  gr_mat_clear(A_inv, ctx);
   gr_mat_clear(G_A, ctx);
   gr_mat_clear(H_A, ctx);
   gr_mat_clear(G_D, ctx);
   gr_mat_clear(H_D, ctx);
+  gr_mat_clear(Check, ctx);
   gr_ctx_clear(ctx);
-  return res;
+  flint_rand_clear(state);
+  return status;
 }
 
 int test_inverse_2x2() {
