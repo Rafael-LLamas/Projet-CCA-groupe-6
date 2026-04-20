1e87613933dacf5d8f441b92ce415941a6c0f380
Arda Barut <aliardabarut@gmail.com>
2026-04-20 15:56:52 +0200

    not-so-naive quadrant packing

--- src/inverse_toeplitz/inverse_toeplitz_aux.c
+++ src/inverse_toeplitz/inverse_toeplitz_aux.c
@@ -107,8 +107,6 @@ int gr_mat_split_quadrants(gr_mat_t G_a, gr_mat_t H_a, gr_mat_t G_b, gr_mat_t H_
   return status;
 }
 
-
-
 int gr_mat_pack_quadrants(gr_mat_t G_D, gr_mat_t H_D, gr_mat_t G_x, gr_mat_t H_x, gr_mat_t G_y, gr_mat_t H_y,
                           gr_mat_t G_z, gr_mat_t H_z, gr_mat_t G_t, gr_mat_t H_t, gr_ctx_t ctx) {
 
@@ -121,118 +119,49 @@ int gr_mat_pack_quadrants(gr_mat_t G_D, gr_mat_t H_D, gr_mat_t G_x, gr_mat_t H_x
   slong ry = gr_mat_ncols(G_y, ctx);
   slong rz = gr_mat_ncols(G_z, ctx);
   slong rt = gr_mat_ncols(G_t, ctx);
-  slong total_rank = rx + ry + rz + rt;
+
+  slong total_rank = rx + ry + rz + rt + 2;
 
   gr_mat_init(G_D, n, total_rank, ctx);
   gr_mat_init(H_D, n, total_rank, ctx);
   status |= gr_mat_zero(G_D, ctx);
   status |= gr_mat_zero(H_D, ctx);
 
-  // not that elegantly pack the quadrants
-  const gr_mat_struct *G_blk[4] = {G_x, G_y, G_z, G_t};
-  const gr_mat_struct *H_blk[4] = {H_x, H_y, H_z, H_t};
-  const slong G_rs[4] = {0, 0, n1, n1};
-  const slong G_rows[4] = {n1, n1, n2, n2};
-  const slong H_rs[4] = {0, n1, 0, n1};
-  const slong H_rows[4] = {n1, n2, n1, n2};
-  const slong col_off[4] = {0, rx, rx + ry, rx + ry + rz};
+  slong col_x = 0;
+  slong col_y = rx;
+  slong col_z = rx + ry;
+  slong col_t = rx + ry + rz;
 
-  for (slong b = 0; b < 4; b++) {
-    slong rk = gr_mat_ncols(G_blk[b], ctx);
-    for (slong k = 0; k < rk; k++) {
-      slong col = col_off[b] + k;
-      for (slong i = 0; i < G_rows[b]; i++) {
-        status |= gr_set(gr_mat_entry_ptr(G_D, G_rs[b] + i, col, ctx), gr_mat_entry_srcptr(G_blk[b], i, k, ctx), ctx);
-      }
-      for (slong j = 0; j < H_rows[b]; j++) {
-        status |= gr_set(gr_mat_entry_ptr(H_D, H_rs[b] + j, col, ctx), gr_mat_entry_srcptr(H_blk[b], j, k, ctx), ctx);
-      }
+  // X
+  for (slong j = 0; j < rx; j++) {
+    for (slong i = 0; i < n1; i++) {
+      status |= gr_set(gr_mat_entry_ptr(G_D, i, col_x + j, ctx), gr_mat_entry_srcptr(G_x, i, j, ctx), ctx);
+      status |= gr_set(gr_mat_entry_ptr(H_D, i, col_x + j, ctx), gr_mat_entry_srcptr(H_x, i, j, ctx), ctx);
     }
   }
-
-  // reverse bleed
-  gr_mat_t v_x, r_x, v_z, r_y, e_last_x, tmp_n1, tmp_n2;
-  gr_mat_init(v_x, n1, 1, ctx);
-  gr_mat_init(r_x, n1, 1, ctx);
-  gr_mat_init(v_z, n2, 1, ctx);
-  gr_mat_init(r_y, n2, 1, ctx);
-  gr_mat_init(tmp_n1, n1, 1, ctx);
-  gr_mat_init(tmp_n2, n2, 1, ctx);
-
-  gr_mat_init(e_last_x, n1, 1, ctx);
-  status |= gr_mat_zero(e_last_x, ctx);
-  status |= gr_one(gr_mat_entry_ptr(e_last_x, n1 - 1, 0, ctx), ctx);
-
-  gr_ptr s_val = gr_heap_init(ctx);
-
-  // boundary bleed by Z
-  status |= gr_mat_mul_vector(tmp_n1, G_x, H_x, e_last_x, ctx);
-  status |= gr_set(s_val, gr_mat_entry_ptr(tmp_n1, n1 - 1, 0, ctx), ctx); // corner
-  status |= gr_mat_apply_Z(v_x, tmp_n1, ctx);
-
-  status |= gr_mat_mul_vector(tmp_n1, H_x, G_x, e_last_x, ctx);
-  status |= gr_mat_apply_Z(r_x, tmp_n1, ctx);
-
-  status |= gr_mat_mul_vector(tmp_n2, G_z, H_z, e_last_x, ctx);
-  status |= gr_mat_apply_Z(v_z, tmp_n2, ctx);
-
-  status |= gr_mat_mul_vector(tmp_n2, H_y, G_y, e_last_x, ctx);
-  status |= gr_mat_apply_Z(r_y, tmp_n2, ctx);
-
-  // cancel
-  gr_mat_t G_corr, H_corr;
-  gr_mat_init(G_corr, n, 2, ctx);
-  gr_mat_init(H_corr, n, 2, ctx);
-  status |= gr_mat_zero(G_corr, ctx);
-  status |= gr_mat_zero(H_corr, ctx);
-
-  gr_ptr temp_calc = gr_heap_init(ctx);
-
-  for (slong i = 0; i < n1; i++) {
-    // y bleed: -v_x * e0^T
-    status |= gr_neg(temp_calc, gr_mat_entry_ptr(v_x, i, 0, ctx), ctx);
-    status |= gr_set(gr_mat_entry_ptr(G_corr, i, 0, ctx), temp_calc, ctx);
-
-    // z bleed: -e0 * r_x^T
-    status |= gr_neg(temp_calc, gr_mat_entry_ptr(r_x, i, 0, ctx), ctx);
-    status |= gr_set(gr_mat_entry_ptr(H_corr, i, 1, ctx), temp_calc, ctx);
+  // Y
+  for (slong j = 0; j < ry; j++) {
+    for (slong i = 0; i < n1; i++)
+      status |= gr_set(gr_mat_entry_ptr(G_D, i, col_y + j, ctx), gr_mat_entry_srcptr(G_y, i, j, ctx), ctx);
+    for (slong i = 0; i < n2; i++)
+      status |= gr_set(gr_mat_entry_ptr(H_D, n1 + i, col_y + j, ctx), gr_mat_entry_srcptr(H_y, i, j, ctx), ctx);
   }
-
-  for (slong i = 0; i < n2; i++) {
-    // t bleed (from z and corner): (-v_z - s_val) * e0^T
-    status |= gr_neg(temp_calc, gr_mat_entry_ptr(v_z, i, 0, ctx), ctx);
-    if (i == 0) status |= gr_sub(temp_calc, temp_calc, s_val, ctx);
-    status |= gr_set(gr_mat_entry_ptr(G_corr, n1 + i, 0, ctx), temp_calc, ctx);
-
-    if (i == 0) {
-      status |= gr_one(gr_mat_entry_ptr(H_corr, n1 + i, 0, ctx), ctx);
-      status |= gr_one(gr_mat_entry_ptr(G_corr, n1 + i, 1, ctx), ctx);
+  // Z
+  for (slong j = 0; j < rz; j++) {
+    for (slong i = 0; i < n2; i++)
+      status |= gr_set(gr_mat_entry_ptr(G_D, n1 + i, col_z + j, ctx), gr_mat_entry_srcptr(G_z, i, j, ctx), ctx);
+    for (slong i = 0; i < n1; i++)
+      status |= gr_set(gr_mat_entry_ptr(H_D, i, col_z + j, ctx), gr_mat_entry_srcptr(H_z, i, j, ctx), ctx);
+  }
+  // T
+  for (slong j = 0; j < rt; j++) {
+    for (slong i = 0; i < n2; i++) {
+      status |= gr_set(gr_mat_entry_ptr(G_D, n1 + i, col_t + j, ctx), gr_mat_entry_srcptr(G_t, i, j, ctx), ctx);
+      status |= gr_set(gr_mat_entry_ptr(H_D, n1 + i, col_t + j, ctx), gr_mat_entry_srcptr(H_t, i, j, ctx), ctx);
     }
-
-    // t bleed (from y): -e0 * r_y^T
-    status |= gr_neg(temp_calc, gr_mat_entry_ptr(r_y, i, 0, ctx), ctx);
-    status |= gr_set(gr_mat_entry_ptr(H_corr, n1 + i, 1, ctx), temp_calc, ctx);
   }
-
-  gr_mat_t G_final, H_final;
-  status |= gr_mat_addition_generateur(G_D, H_D, G_corr, H_corr, G_final, H_final, ctx);
-  gr_mat_swap(G_D, G_final, ctx);
-  gr_mat_swap(H_D, H_final, ctx);
+  
   status |= gr_mat_generator_compress(G_D, H_D, ctx);
-
-  gr_mat_clear(G_final, ctx);
-  gr_mat_clear(H_final, ctx);
-  gr_mat_clear(G_corr, ctx);
-  gr_mat_clear(H_corr, ctx);
-  gr_mat_clear(v_x, ctx);
-  gr_mat_clear(r_x, ctx);
-  gr_mat_clear(v_z, ctx);
-  gr_mat_clear(r_y, ctx);
-  gr_mat_clear(tmp_n1, ctx);
-  gr_mat_clear(tmp_n2, ctx);
-  gr_mat_clear(e_last_x, ctx);
-  gr_heap_clear(s_val, ctx);
-  gr_heap_clear(temp_calc, ctx);
-
+  
   return status;
 }
