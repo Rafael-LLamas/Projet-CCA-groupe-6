620cc2c3583bcc5977b4424bc93afb893a464e66
Arda Barut <aliardabarut@gmail.com>
2026-06-19 16:45:10 +0300

    gr_struct_mat_guess_struct_t init

--- src/gr_struct_mat/utility/guess_struct_t.c
+++ src/gr_struct_mat/utility/guess_struct_t.c
@@ -7,23 +7,70 @@
 #include <stdlib.h>
 #include <time.h>
 
-// Guess the structure type of the matrix mat, return it on type based off the accuracy rate in [0,1]
-int _gr_struct_mat_guess_struct_t(structure_type_t type, gr_mat_t mat, slong acc_rate, gr_ctx_t ctx)
+#include <math.h>
+
+#include "../../gr_struct_mat.h"
+#include "flint/flint.h"
+#include "flint/gr.h"
+#include "flint/gr_mat.h"
+#include "flint/gr_types.h"
+
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+
+int _gr_struct_mat_guess_struct_t(structure_type_t *type, gr_mat_t mat, float acc_rate, gr_ctx_t ctx)
 {
   int res = GR_SUCCESS;
 
-  if (acc_rate > 1) acc_rate = 1;
+  if (type == NULL) return GR_DOMAIN;
+
+  if (acc_rate > 1) acc_rate = 1.0f;
   if (acc_rate < 0)
   {
-    res = T_TOEPLITZ;
+    *type = T_TOEPLITZ;
     return res;
   }
 
-  slong nb_test = gr_mat_nrows(mat, ctx) * gr_mat_ncols(mat, ctx) * acc_rate;
+  slong rows = gr_mat_nrows(mat, ctx);
+  slong cols = gr_mat_ncols(mat, ctx);
+
+  // if matrix is 1x1 then assume Toeplitz but no computational advantage possible
+  if (rows <= 1 || cols <= 1)
+  {
+    *type = T_TOEPLITZ;
+    return res;
+  }
+
+  // A better approach possible on checking only req_check elements rather than O(n^2)
+  // Careful on selecting "uniformly" random elemnets without a quantum machine
+  // https://arxiv.org/pdf/math-ph/0609050
+  // Maybe use Haar measure?
+
+  slong nb_check = (rows - 1) * (cols - 1);
+  slong req_check = (slong)round((double)nb_check * acc_rate);
+
   slong c_toeplitz = 0;
   slong c_hankel = 0;
 
-  // https://arxiv.org/pdf/math-ph/0609050
-  // Haar measure
-  // Might wanna do something smart here
+  for (slong i = 0; i < rows - 1; i++)
+  {
+    for (slong j = 0; j < cols - 1; j++)
+    {
+      gr_ptr toep_curr = gr_mat_entry_ptr(mat, i, j, ctx);
+      gr_ptr toep_next = gr_mat_entry_ptr(mat, i + 1, j + 1, ctx);
+      if (gr_equal(toep_curr, toep_next, ctx) == T_TRUE) { c_toeplitz++; }
+
+      gr_ptr hank_curr = gr_mat_entry_ptr(mat, i + 1, j, ctx);
+      gr_ptr hank_next = gr_mat_entry_ptr(mat, i, j + 1, ctx);
+      if (gr_equal(hank_curr, hank_next, ctx) == T_TRUE) { c_hankel++; }
+    }
+  }
+
+  if (c_toeplitz >= req_check) { *type = T_TOEPLITZ; }
+  else if (c_hankel >= req_check) { *type = T_HANKEL; }
+  else
+    *type = T_UNSURE;
+
+  return res;
 }
